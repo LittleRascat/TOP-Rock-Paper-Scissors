3dac3eeb033e466c2a9f7bf9f68f81291c30eca6
Roman <royr0212@gmail.com>
2021-03-14 12:19:21 -0500

    Added win counter to game and shuffled playround conditions.

--- script.js
+++ script.js
@@ -8,33 +8,51 @@ function computerPlay() {
 
 //Play round. Output win or lose statement.
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() == 'rock' && computerSelection == '1') {
-    return 'You Lose! Paper Beats Rock';
-  } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == '0') {
-    return 'You Win! Paper Beats Rock';
-  } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == '2') {
-    return 'You Win! Rock Beats Scissors';
-  } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == '0') {
-    return 'You Lose! Rock Beats Scissors';
-  } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == '2') {
-    return 'You Lose! Scissors Beats Paper';
-  } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == '1') {
-    return 'You Win! Scissors Beats Paper';
+  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 1) {
+    let winLose = 'Lose';
+    return 'You ' + winLose + '! Paper Beats Rock';
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 0) {
+    let winLose = 'Win';
+    return 'You ' + winLose + '! Paper Beats Rock';
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 2) {
+    let winLose = 'Win';
+    return 'You ' + winLose + '! Rock Beats Scissors';
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 0) {
+    let winLose = 'Lose';
+    return 'You ' + winLose + '! Rock Beats Scissors';
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 2) {
+    let winLose = 'Lose';
+    return 'You ' + winLose + '! Scissors Beats Paper';
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 1) {
+    let winLose = 'Win';
+    return 'You ' + winLose + '! Scissors Beats Paper';
   } else {
-    return 'It\'s a draw!';
+    let winLose = 'draw';
+    return 'It\'s a ' + winLose + '!';
   }
 }
 
-const playerSelection = "RoCk";
+const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+console.log(computerPlay())
 
 //Play round five times.
 function game() {
+  let winCount = 0;
+  let winLose = '';
   for (i = 0; i < 5; i++) {
     const playerSelection = prompt("Choose Rock, Paper, or Scissors:");
     const computerSelection = computerPlay();
-    computerPlay();
     console.log(playRound(playerSelection, computerSelection));
+    console.log(computerPlay())
+    if (winLose === 'Win') {
+      winCount += 1;
+    }
+  }
+  if (winCount >= 3) {
+    return 'Congradulations, you won the game!';
+  } else {
+    return 'Bad luck, you lost tne game!';
   }
 }
